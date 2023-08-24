ddf4954c4495209d52109d6ba81120e6ebaf3fa4
Anton Karpowitsch <karpowitschanton@gmail.com>
2023-08-24 11:10:24 +0200

    LiveCalc

--- JavaScript/JSForms-2/js-forms-2_simple-live-calculator/js/index.js
+++ JavaScript/JSForms-2/js-forms-2_simple-live-calculator/js/index.js
@@ -3,3 +3,15 @@ console.clear();
 const firstInput = document.querySelector('[data-js="firstValue"]');
 const secondInput = document.querySelector('[data-js="secondValue"]');
 const result = document.querySelector('[data-js="result"]');
+
+
+
+firstInput.addEventListener('input', updateResult);
+
+secondInput.addEventListener('input', updateResult);
+
+function updateResult() {   
+const endResult = firstInput.value * secondInput.value;
+
+result.textContent = `${endResult}`;
+}
