c70ae8f9dd4e6920affe07bc40876a0a90f00b22
Anton Karpowitsch <karpowitschanton@gmail.com>
2023-08-24 10:43:59 +0200

    uppercase

--- JavaScript/JSForms-2/js-forms-2_controlled-input-uppercase/js/index.js
+++ JavaScript/JSForms-2/js-forms-2_controlled-input-uppercase/js/index.js
@@ -1,3 +1,11 @@
 console.clear();
 
 const input = document.querySelector('[data-js="toUpperCase"]');
+
+
+
+input.addEventListener('input', (event)=> {
+event.preventDefault();
+
+input.value = input.value.toUpperCase();
+});
