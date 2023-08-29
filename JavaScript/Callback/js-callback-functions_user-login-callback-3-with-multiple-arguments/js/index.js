99163578c4ac393098f1191a725c07fa625a680f
Anton Karpowitsch <karpowitschanton@gmail.com>
2023-08-29 11:33:51 +0200

    allDone

--- JavaScript/Callback/js-callback-functions_user-login-callback-3-with-multiple-arguments/js/index.js
+++ JavaScript/Callback/js-callback-functions_user-login-callback-3-with-multiple-arguments/js/index.js
@@ -4,4 +4,12 @@ function handleUserLogin(onSuccess) {
   onSuccess("Jane Doe", "admin");
 }
 
+function showWelcomeMessage(userName, userRole) {
+console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
 // The exercise starts here!
+};
+handleUserLogin(showWelcomeMessage);
+
+handleUserLogin(function(userName, userRole) {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}`);
+});
