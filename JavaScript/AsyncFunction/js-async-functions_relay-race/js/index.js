43ba1c16b0df419fd79a4cb57c9695b2db4b0c36
Anton Karpowitsch <karpowitschanton@gmail.com>
2023-09-01 10:56:55 +0200

    animateRunner

--- JavaScript/AsyncFunction/js-async-functions_relay-race/js/index.js
+++ JavaScript/AsyncFunction/js-async-functions_relay-race/js/index.js
@@ -8,15 +8,18 @@ const startRaceButton = document.querySelector('[data-js="start-race-button"]');
 
 // You'll need to change something about this callback function:
 //                                         ↙️
-startRaceButton.addEventListener("click", () => {
+startRaceButton.addEventListener("click", async () => {
   startRaceButton.setAttribute("disabled", "");
 
   /**
    * Hint 1:
    * The relay runners are already available in the global scope as `runner1`,
    * `runner2` and `runner3`.
-   *
-   * Hint 2:
+   */
+    await animateRunner(runner1);
+    await animateRunner(runner2);
+    await animateRunner(runner3);
+  /* Hint 2:
    * Use `animateRunner(runnerX)` to animate the runners.
    *
    * Hint 3:
