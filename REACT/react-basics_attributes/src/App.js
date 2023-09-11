762625769c21de4f0d324875af864a7ea980f454
Anton Karpowitsch <karpowitschanton@gmail.com>
2023-09-11 11:37:48 +0200

    react

--- REACT/react-basics_attributes/src/App.js
+++ REACT/react-basics_attributes/src/App.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return Article();
+}
+
+function Article() {
+  return (<article className="article">
+<h2 className="article__title">Life aaah finds a Way</h2>
+<label htmlFor="react" id="react">How much do you love Jurassicpark?</label>
+<input type="text" id="react"></input>
+<a href="https://www.jurassicpark.com/" className="article__link">Jurassicpark</a>
+  </article>);
 }
