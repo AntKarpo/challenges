178329dd9fe51ee8f9c2066a3ba6ba213c36ebd5
Anton Karpowitsch <karpowitschanton@gmail.com>
2023-10-06 13:53:48 +0200

    done

--- BackEnd-Api-Routes/backend-api-routes_products/pages/products/[id].js
+++ BackEnd-Api-Routes/backend-api-routes_products/pages/products/[id].js
@@ -1,18 +1,26 @@
 import { useRouter } from 'next/router';
+import useSWR from 'swr';
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 export default function ProductID() {
   const router = useRouter();
   const { id } = router.query;
 
-  if (!id) {
+  const { data, isLoading } = useSWR(`/api/products/${id}`, fetcher);
+
+  if (isLoading) {
     return <div>Loading...</div>;
   }
-
-
+console.log(data);
   return (
-    <div>
-      {id}
-    </div>
+    <>
+    <h1>
+      {data.name}
+    </h1>
+    <p>{data.description}</p>
+    <p>{data.price} $</p>
+</>
   );
 }
 
