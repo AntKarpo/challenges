68708dd8353cbdf349e65177db081ed11beef992
Anton Karpowitsch <karpowitschanton@gmail.com>
2023-10-06 09:31:49 +0200

    1stOneDone

--- BACKEND/backend-basics_greeting/server.js
+++ BACKEND/backend-basics_greeting/server.js
@@ -1,3 +1,10 @@
 import { createServer } from "node:http";
 
-// export const server = …
+ export const server = createServer((request, response)=>{
+   if (request.url === "/") {
+    response.statusCode= 200;
+    response.end("Hello Anton");
+ } else {
+    response.statusCode = 404;
+    response.end("Not Found");
+ }})
